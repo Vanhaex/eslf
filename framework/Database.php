2ed150c54959e011120cdfda805b4e8f0ed70666
Vanhaex <monlouis59@hotmail.fr>
2021-05-05 19:07:17 +0200

    Adding more functions in Database class

--- framework/Database.php
+++ framework/Database.php
@@ -142,15 +142,137 @@ class Database
     }
   }
 
+  public function preparedQuery(string $prepared_query, string $bind_param, ...$bind_data)
+  {
+    this->reset_mysqli(); // Empty the variables
+
+    trim($prepared_query); // Remove spaces
+
+    $stmt = $this->mysqli->prepare($prepared_query);
+
+    if ($stmt == true) {
+      $stmt->bind_param($bind_param, $bind_data);
+
+      $exec_preapred_query = $stmt->execute();
+
+      if ($exec_preapred_query == true) {
+        if ($this->mysqli->field_count) {
+          $result = $this->mysqli->result_metadata();
+
+          if ($this->result_object == true) {
+            while ($data = $result->fetch_object()) {
+              array_push($this->result, $data);
+            }
+          }
+          else {
+            while ($data = $result->fetch_assoc()) {
+              array_push($this->result, $data);
+            }
+          }
+
+          $this->row_count = $this->mysqli->num_rows; // Getting the number of rows
+
+          $this->result->free(); // Free memory
+        }
+        else{
+          $this->affected_rows = $this->mysqli->affected_rows;
+          $this->last_id = $this->mysqli->last_id;
+        }
+      }
+      else {
+        $this->err_code = $this->mysqli->errno;
+        $this->err_string = $this->mysqli->error;
+        printf("An error occured during the execution of prepared query");
+        exit();
+      }
+    }
+    else {
+      $this->err_code = $this->mysqli->errno;
+      $this->err_string = $this->mysqli->error;
+      printf("The prepared query is incorrect");
+      exit();
+    }
+
+    $stmt->close();
+  }
+
+  public function getAllResults()
+  {
+    return $this->result;
+  }
+
+  public function getNextResult()
+  {
+    $next_result = current($this->result);
+    next($next_result);
+
+    if ((!is_array($next_result)) || (!is_object($next_result))) {
+      printf("Error : the result is not an array or an object");
+      exit();
+    }
+  }
+
+  public function getNumberResults()
+  {
+    return $this->num_of_rows;
+  }
+
+  public function getLastID()
+  {
+    return $this->insert_id;
+  }
+
+  public function getErrorCode()
+  {
+    return $this->err_code;
+  }
+
+  public function getErrorString()
+  {
+    return $this->err_string;
+  }
+
   public function reset_mysqli()
   {
-      // Let's clean this variables
+      // Let's clean the variables
       $this->err_code = 0;
       $this->err_string = null;
       $this->result_store = 0;
       $this->insert_id = 0;
       $this->affected_rows = 0;
       $this->num_of_rows = 0;
+  }
+
+  /**** Transaction options ****/
+
+  public function commit()
+  {
+    if ($this->transaction == true) {
+      return $this->mysqli->commit();
+    }
+    else {
+      printf("Impossible to commit : Transaction mode set to false");
+    }
+  }
+
+  public function rollback()
+  {
+    if ($this->transaction == true) {
+      return $this->mysqli->rollback();
+    }
+    else {
+      printf("Impossible to rollback : Transaction mode set to false");
+    }
+  }
+
+  public function autocommit()
+  {
+    if ($this->transaction == true) {
+      return $this->mysqli->autocommit(true);
+    }
+    else {
+      return $this->mysqli->autocommit(false);
+    }
   }
 }
 
