eaa24322436eb692a2138c223b507052d92abe96
Vanhaex <monlouis59@hotmail.fr>
2021-06-28 17:09:26 +0200

    Update Router.php

--- framework/Router.php
+++ framework/Router.php
@@ -2,6 +2,7 @@
 
 namespace Framework;
 
+use Framework\InputUtility;
 use Framework\Route;
 use Framework\RouteException;
 
@@ -9,7 +10,9 @@ class Router
 {
 
     private $path; // Contiendra l'URL sur laquelle on souhaite se rendre
-    private $routes = []; // Contiendra la liste des routes
+    private $routes = ['GET' => [], 'POST' => []];
+
+    public function __construct(){}
 
     public function get($path, $controller, $method)
     {
@@ -25,17 +28,30 @@ class Router
       return $route; // On retourne la route pour "enchainer" les méthodes
     }
 
+    private function execute_route($request_method, $url)
+    {
+      if (in_array($request_method, ['GET', 'POST'])) {
+        $existing_routes = $this->routes[$request_method];
+        foreach ($existing_routes as $route) {
+          $result = $route->match($url);
+          if (is_array($result) == false)
+          {
+            continue;
+          }
+          return $route->call($result);
+        }
+      }
+
+      throw new RouteException;
+    }
+
     public function run()
     {
-      if(!isset($this->routes[$_SERVER['REQUEST_METHOD']])){
-          throw new RouteException('REQUEST_METHOD est inexistante');
-      }
-      foreach($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
-          if($route->match($this->path)){
-              return $route->call();
-          }
-      }
-      throw new RouteException('Aucune route');
+      $req_method = InputUtility::request_method();
+      $clean_path = InputUtility::clean_uri();
+
+      // On a vérifié la méthode utilisée et l'URI est propre
+      return $this->execute_route($req_method, $clean_path);
     }
 
 }
