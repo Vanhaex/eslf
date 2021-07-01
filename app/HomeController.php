abe6c74ce2ec3d686ed3fa15945a7f640d1b099b
Vanhaex <monlouis59@hotmail.fr>
2021-07-01 16:04:32 +0200

    Update HomeController.php

--- app/HomeController.php
+++ app/HomeController.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Framework\InputUtility;
 use Framework\Controller;
+use Framework\Database;
 
 class HomeController extends Controller
 {
+
   public function index()
   {
     $this->view('home.tpl');
@@ -20,6 +22,31 @@ class HomeController extends Controller
   public function test($id)
   {
     echo "Le ID est égal à : " . $id;
+  }
+
+  public function database()
+  {
+    $this->view('database.tpl', $params);
+  }
+
+  public function database_prepared()
+  {
+    $name = InputUtility::post('name');
+
+    $database = new Database();
+
+    $database->preparedQuery("SELECT id, name FROM test_table WHERE name = ?", "s", $name);
+
+    $result = $database->getNextResult();
+
+    $number = $database->getNumberResults();
+
+    if ($number == 0) {
+      echo "Y'a rien du tout !";
+    }
+    else {
+      echo "Il y a une entrée : " . $result->name;
+    }
   }
 }
 
