7c7d34e7ef3c8b566c7bdfbfbe595bbf3fe8a280
Vanhaex <monlouis59@hotmail.fr>
2021-08-04 12:28:09 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -23,6 +23,10 @@ require_once("../vendor/autoload.php");
 // Smarty Class
 require_once("../vendor/smarty/smarty/libs/Smarty.class.php");
 
+// Config files
+include("../config/routes.config.php");
+include("../config/app.config.php");
+
 use Framework\Router;
 use Framework\Controller;
 
@@ -32,7 +36,11 @@ try
 {
   $router = new Router();
 
-  include("../config/routes.config.php");
+  if (CONFIG_DEBUG == "true"){
+      ini_set('display_errors', 1);
+      ini_set('display_startup_errors', 1);
+      error_reporting(E_ALL);
+  }
 
   $render = $router->run();
   echo $render;
@@ -43,13 +51,16 @@ catch(\Framework\RouteException $e)
   $smarty->setCompileDir($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .  'views_c' . DIRECTORY_SEPARATOR);
   $smarty->display($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "errors" . DIRECTORY_SEPARATOR . "404.tpl");
 }
-catch(\Exception $e)
+catch(Exception $e)
 {
   header("HTTP/1.1 505 Internal Server Error");
+  $smarty->setCompileDir($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .  'views_c' . DIRECTORY_SEPARATOR);
+  
+  // Affichage du message si erreur 500
+  if(CONFIG_DEBUG == "true"){
+    $smarty->assign('detail_exception', 'Détails de l\'erreur : ' . $e->getFile() . ':' . $e->getLine() . '<br>' . $e->getMessage());
+  }
   $smarty->display($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "errors" . DIRECTORY_SEPARATOR . "500.tpl");
 }
-
-
-
 
 ?>
