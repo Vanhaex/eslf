a2562f7a5fd0ee67920c46cf4fd4273c8dfab004
Vanhaex <monlouis59@hotmail.fr>
2021-11-05 16:40:48 +0100

    supp fonction inutilisée

--- app/HomeController.php
+++ app/HomeController.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Framework\InputUtility;
 use Framework\Controller;
-use Framework\Database;
 
 class HomeController extends Controller
 {
@@ -24,30 +23,19 @@ class HomeController extends Controller
     echo "Le ID est égal à : " . $id;
   }
 
-  public function database()
+  public function testdb()
   {
+    $this->esdbaccess->querySelect("test_table", null, null);
+
+    $resultat = $this->esdbaccess->allResults();
+
+    $params = [
+        "resultat" => $resultat
+    ];
+
     $this->view('database.tpl', $params);
   }
 
-  public function database_prepared()
-  {
-    $name = InputUtility::post('name');
-
-    $database = new Database();
-
-    $database->preparedQuery("SELECT id, name FROM test_table WHERE name = ?", "s", $name);
-
-    $result = $database->getNextResult();
-
-    $number = $database->getNumberResults();
-
-    if ($number == 0) {
-      echo "Y'a rien du tout !";
-    }
-    else {
-      echo "Il y a une entrée : " . $result->name;
-    }
-  }
 }
 
 
