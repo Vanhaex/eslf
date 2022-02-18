e7d6b1a041f0fd5a669bc478ca46e601445f9c7e
Vanhaex <monlouis59@hotmail.fr>
2022-02-18 18:41:43 +0100

    [EDIT] Suppression des frameworks js et fontawesome

--- config/app.example.config.php
+++ config/app.example.config.php
@@ -1,12 +1,12 @@
 <?php
 
 // DEV, TEST or PROD
-define('CONFIG_ENVIRONMENT', 'dev');
+define('EX_CONFIG_ENVIRONMENT', 'dev');
 
 // Debug mode, pour voir les erreurs
-define('CONFIG_DEBUG', 'true');
+define('EX_CONFIG_DEBUG', 'true');
 
 // Le chemin où sont stockés les logs (dans web/log par défaut)
-define('LOG_FILE_PATH', '/chemin/vers/le/dossier/log/'); // Toujours avec "/" à la fin
+define('EX_LOG_FILE_PATH', '/chemin/vers/le/dossier/log/'); // Toujours avec "/" à la fin
 
 ?>
--- config/database.example.config.php
+++ config/database.example.config.php
@@ -1,14 +1,14 @@
 <?php
 
-define('CONFIG_DATABASE_HOST', 'localhost');
-define('CONFIG_DATABASE_PORT', 3306);
-define('CONFIG_DATABASE_USER', 'root');
-define('CONFIG_DATABASE_PASSWORD', 'password');
-define('CONFIG_DATABASE_DATABASE', 'database_name_example');
+define('EX_CONFIG_DATABASE_HOST', 'localhost');
+define('EX_CONFIG_DATABASE_PORT', 3306);
+define('EX_CONFIG_DATABASE_USER', 'root');
+define('EX_CONFIG_DATABASE_PASSWORD', 'password');
+define('EX_CONFIG_DATABASE_DATABASE', 'database_name_example');
 
 // Database parameters
-define('CONFIG_DATABASE_EXCEPTIONS', 'true'); // Enable exceptions
-define('CONFIG_DATABASE_TRANSACTION', 'true'); // Enable transaction
-define('CONFIG_DATABASE_FETCH_OBJECT', 'true'); // Result as object (false = associative array)
+define('EX_CONFIG_DATABASE_EXCEPTIONS', 'true'); // Enable exceptions
+define('EX_CONFIG_DATABASE_TRANSACTION', 'true'); // Enable transaction
+define('EX_CONFIG_DATABASE_FETCH_OBJECT', 'true'); // Result as object (false = associative array)
 
 ?>
